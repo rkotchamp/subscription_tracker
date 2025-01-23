d1327543299ec4acc2762a42c12ba62c3ac8f78e
Reuben <treuben90@yahoo.com>
2025-01-24 01:24:16 +0200

    new sidebars

--- app/lib/utils/index.jsx
+++ app/lib/utils/index.jsx
@@ -1,1 +1,7 @@
- 
+// Export everything from utils.jsx
+export { cn } from "./utils";
+
+// If you add more utility functions in the future, you can export them here too
+// For example:
+// export { formatDate } from "./date-utils";
+// export { validateEmail } from "./validation-utils";
