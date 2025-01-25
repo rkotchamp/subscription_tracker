b34d08df95bbd10d713eb85c49916535ef4ca4d3
Reuben <treuben90@yahoo.com>
2025-01-25 21:21:25 +0200

    created the user tab

--- app/components/ui/Sidebar/UIs/quick-access.jsx
+++ app/components/ui/Sidebar/UIs/quick-access.jsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { Bell, MoreHorizontal, Download, Share2 } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import {
   SidebarGroup,
   SidebarGroupLabel,
   SidebarMenu,
-  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar";
 
 export function QuickAccess({ items }) {
-  const { isMobile } = useSidebar();
-
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarGroupLabel>Quick Access</SidebarGroupLabel>
@@ -33,33 +21,6 @@ export function QuickAccess({ items }) {
                 <span>{item.name}</span>
               </a>
             </SidebarMenuButton>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuAction showOnHover>
-                  <MoreHorizontal />
-                  <span className="sr-only">More</span>
-                </SidebarMenuAction>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-48 rounded-lg"
-                side={isMobile ? "bottom" : "right"}
-                align={isMobile ? "end" : "start"}
-              >
-                <DropdownMenuItem>
-                  <Download className="text-muted-foreground" />
-                  <span>Download</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Share2 className="text-muted-foreground" />
-                  <span>Share</span>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Bell className="text-muted-foreground" />
-                  <span>Remind Me</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
           </SidebarMenuItem>
         ))}
       </SidebarMenu>
